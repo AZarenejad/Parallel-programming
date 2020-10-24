5afb9b75ccba2f6092cd1a5d1e65a873969934cf
Alireza Zarenejad <alireza.zarenejad99@gmail.com>
2020-10-24 17:38:07 +0330

    change

--- ca1/part2.cpp
+++ ca1/part2.cpp
@@ -46,6 +46,7 @@ void print_u16 (__m128i a)
 
 void print_u32 (__m128i a)
 {
+	printf ("Unsigned double word: ");
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
@@ -57,6 +58,7 @@ void print_u32 (__m128i a)
 
 void print_u64 (__m128i a)
 {
+	printf ("Unsigned quad word: ");
 	intVec tmp;
 	tmp.int128 = a;
 	printf ("[");
@@ -64,6 +66,55 @@ void print_u64 (__m128i a)
 		printf ("%X, ", tmp.m128_u64[i]);
 	}
 	printf ("%X]\n\n", tmp.m128_u64[0]);
+}
+
+
+void print_i8 (__m128i a)
+{
+	printf ("Signed byte: ");
+	intVec tmp;
+	tmp.int128 = a;
+	printf ("[");
+	for (int i=15; i>0; i--) {
+		printf ("%d, ", tmp.m128_i8[i]);
+	}
+	printf ("%d]\n\n", tmp.m128_i8[0]);
+}
+
+void print_i16 (__m128i a)
+{
+	printf ("Signed word: ");
+	intVec tmp;
+	tmp.int128 = a;
+	printf ("[");
+	for (int i=7; i>0; i--) {
+		printf ("%d, ", tmp.m128_i16[i]);
+	}
+	printf ("%d]\n\n", tmp.m128_i16[0]);
+}
+
+void print_i32 (__m128i a)
+{
+	printf ("Signed double word: ");
+	intVec tmp;
+	tmp.int128 = a;
+	printf ("[");
+	for (int i=3; i>0; i--) {
+		printf ("%d, ", tmp.m128_i32[i]);
+	}
+	printf ("%d]\n\n", tmp.m128_i32[0]);
+}
+
+void print_i64 (__m128i a)
+{
+	printf ("Signed quad word: ");
+	intVec tmp;
+	tmp.int128 = a;
+	printf ("[");
+	for (int i=1; i>0; i--) {
+		printf ("%d, ", tmp.m128_i64[i]);
+	}
+	printf ("%d]\n\n", tmp.m128_i64[0]);
 }
 
 
@@ -78,8 +129,24 @@ void print_int_vector(__m128i a, string type)
 		print_u32(a);
 	else if(type == "u64")
 		print_u64(a);
+	else if(type == "i8")
+		print_i8(a);
+	else if(type == "i16")
+		print_i16(a);
+	else if(type == "i32")
+		print_i32(a);
+	else if (type == "i64")
+		print_i64(a);
+	else
+		printf("unknown type\n");
+	
 }
 
+
+
+void print_spfp_vector(__m128 a)
+{
+}
 
 unsigned char intArray [16] = {	0X00, 0X11, 0X22, 0X33, 0X44, 0X55, 0X66, 0X77,
 								0X88, 0X99, 0XAA, 0XBB, 0XCC, 0XDD, 0XEE, 0XFF};
@@ -96,7 +163,14 @@ int main(void)
 	print_int_vector(a, "u32");
 
 	print_int_vector(a, "u64");
-	
+
+	print_int_vector (a , "i8");
+
+	print_int_vector (a , "i16");
+
+	print_int_vector(a, "i32");
+
+	print_int_vector(a, "i64");
 
 	return 0;
 }
