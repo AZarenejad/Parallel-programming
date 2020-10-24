90d2241eb94e00f9aa508ebbb2e3a1c0d79accb3
Alireza Zarenejad <alireza.zarenejad99@gmail.com>
2020-10-24 20:03:37 +0330

    final

--- ca1/part2b.cpp
+++ ca1/part2b.cpp
@@ -20,14 +20,20 @@ void print_spfp_vector(__m128 a)
 	printf ("%f]\n\n", tmp.sp[0]);
 }
 
-float floatArray [4] = {1.1, 2.2, 3.3, 4.4};
+float floatArray [4] = {1.1, 1.5, 2.2, 2.7};
+
+unsigned char floatArray1 [16] = { 0XCD, 0XCC, 0X8C, 0X3F, 0XCD, 0XCC, 0X0C, 0X40,
+								0X33, 0X33, 0X53, 0X40, 0XCD, 0XCC, 0X8C, 0X40};
 
 
 int main()
 {
     __m128 a;
+    __m128 b;
 
-	a = _mm_load_ps(floatArray);
+	a = _mm_load_ps((const float*)floatArray1);
+    b = _mm_load_ps(floatArray);
 
     print_spfp_vector(a);
+    print_spfp_vector(b);
 }
