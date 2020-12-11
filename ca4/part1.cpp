0fc965563b80c1d1ab03eb3eb2a3a9d34596481c
Alireza Zarenejad <alireza.zarenejad99@gmail.com>
2020-12-11 20:13:34 +0330

    add critical and lock for part1 omp

--- ca4/part1.cpp
+++ ca4/part1.cpp
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "omp.h"
 #include "ipp.h"
+#include <float.h>
 #include <algorithm>
 #include <random>
 
@@ -59,7 +60,81 @@ int serial_max(float* array) {
 }
 
 
-int omp_max(float* array) {
+int omp_lock_max(float* array)
+{   
+	Ipp64u start, end;
+	int omp_lock_duration;
+	omp_lock_t lock;
+	omp_init_lock(&lock);
+
+	start = ippGetCpuClocks();
+
+	float max_value = array[0];
+	long max_index = 0;
+
+	#pragma omp parallel for num_threads(4)
+	for (long i = 0; i < VECTOR_SIZE; i++){
+		if(array[i] > max_value) {
+			omp_set_lock(&lock);
+				max_value = array[i];
+				max_index = i;
+			omp_unset_lock(&lock);
+		}
+	}
+
+	omp_destroy_lock(&lock);
+	end = ippGetCpuClocks();
+    omp_lock_duration = (Ipp32s)(end - start);
+
+	printf("Find Max with omp lock:	Max value = %f,  Index = %ld, Run time = %d\n", max_value, max_index, omp_lock_duration);
+    return omp_lock_duration;
+}
+
+
+long opm_critical_max(float* array)
+{   
+	Ipp64u start, end;
+	int omp_critical_duration;
+
+	start = ippGetCpuClocks();
+	float max_value = array[0];
+	long max_index = 0;
+	float max_value_local;
+	long max_index_local;
+
+	#pragma omp parallel num_threads(2) shared(array, max_value, max_index) private(max_value_local, max_index_local)
+	{
+		max_value_local = FLT_MIN;
+		max_index_local = -1;
+
+		#pragma omp for nowait
+		for (long i = 0; i < VECTOR_SIZE; i++){
+			if (array[i] > max_value_local){
+				max_value_local = array[i];
+				max_index_local = i;
+			}
+		}
+
+		#pragma omp critical
+		{
+			if(max_value_local > max_value){
+				max_value = max_value_local;
+				max_index = max_index_local;
+			}
+		}
+	}
+
+	end = ippGetCpuClocks();
+    omp_critical_duration = (Ipp32s)(end - start);
+
+
+	printf("Find Max with omp crttical:	Max value = %f,  Index = %ld, Run time = %d\n", max_value, max_index, omp_critical_duration);
+    return omp_critical_duration;
+
+}
+
+
+int omp_max_reduction_without_index(float* array) {
 	Ipp64u start, end;
 	int omp_duration;
 	start = ippGetCpuClocks();
@@ -71,8 +146,42 @@ int omp_max(float* array) {
 	
 	end = ippGetCpuClocks();
     omp_duration = (Ipp32s)(end - start);
-	printf("Find Max in omp: Max value = %f, Run time = %d\n", max_value, omp_duration);
+	printf("Find Max in omp reduction without index: Max value = %f, Run time = %d\n", max_value, omp_duration);
     return omp_duration;
+}
+
+
+struct find_max {
+	float val;
+	long index; 
+};
+
+#pragma omp declare reduction(maximum : struct find_max : omp_out = omp_in.val > omp_out.val ? omp_in : omp_out)
+
+long omp_max_with_reduction_declaration(float* array)
+{   
+
+	Ipp64u start, end;
+	int omp_reduction_duration;
+
+	start = ippGetCpuClocks();
+
+	struct find_max max;
+	max.val = FLT_MIN;
+	max.index = -1;
+
+	#pragma omp parallel for num_threads(4) reduction(maximum:max)
+	for (long i = 0; i < VECTOR_SIZE; i++){
+		if (array[i] > max.val){
+			max.val = array[i];
+			max.index = i;
+		}
+	}
+
+	end = ippGetCpuClocks();
+   	omp_reduction_duration = (Ipp32s)(end - start);
+	printf("Find Max in omp reduction: Max value = %f, Max Index: %ld, Run time = %d\n", max.val, max.index, omp_reduction_duration);
+    return omp_reduction_duration;
 }
 
 
@@ -85,9 +194,21 @@ int main(int argc, char const *argv[])
 	
 	int serial_duration = serial_max(random_array);
 
-	int omp_duration = omp_max(random_array);
+	int omp_lock_duration = omp_lock_max(random_array);
+
+	int omp_critical_duration = opm_critical_max(random_array);
+
+	int omp_duration_reduction_without_index = omp_max_reduction_without_index(random_array);
+
+	int omp_reduction_duration = omp_max_with_reduction_declaration(random_array);
 
 
-    printf("Speed up: %f\n", ((float)serial_duration/(float)omp_duration));
+    printf("Speed up with omp lock: %f\n", ((float)serial_duration/(float)omp_lock_duration));
+
+	printf("Speed up with omp critical: %f\n", ((float)serial_duration/(float)omp_critical_duration));
+
+	printf("Speed up with omp reduction without index: %f\n", ((float)serial_duration/(float)omp_duration_reduction_without_index));
+
+	printf("Speed up with omp reduction declaration: %f\n", ((float)serial_duration/(float)omp_reduction_duration));
 
 }
